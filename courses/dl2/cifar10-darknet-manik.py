d0c5e0d5ea1e25e53d0ff708dfaacc185f3e5dd1
Manik <technophile21@gmail.com>
2019-06-01 13:36:26 +0530

    python files till Lecture 12

---
+++ courses/dl2/cifar10-darknet-manik.py
@@ -0,0 +1,15 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.3'
+#       jupytext_version: 1.0.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+
--- courses/dl2/pascal-manik__old.py
+++ courses/dl2/pascal-manik__old.py
@@ -25,7 +25,7 @@ import json
 from PIL import ImageDraw, ImageFont
 from matplotlib import patches, patheffects
 
-
+torch.cuda.set_device(0)
 # -
 
 PATH = Path('../../../data/pascal')
@@ -165,162 +165,22 @@ JPEGS = 'VOCdevkit/VOC2007/JPEGImages'
 tfm_model = tfms_from_model(f_model, sz, aug_tfms=transforms_side_on, crop_type=CropType.NO)
 md = ImageClassifierData.from_csv(PATH, JPEGS, CSV, tfms=tfm_model, bs=bs)
 
-md
-
-#get the iterator from data loader and extract the next batch
 x,y = next(iter(md.val_dl))
 
 
-#x is normalized (done during application of transforms)
-# we use md.val_ds to denormalize as we want to use the same stats for normalization as used for normalization
 show_img(md.val_ds.denorm(to_np(x))[0]);
 
 learn = ConvLearner.pretrained(f_model, md, metrics=[accuracy])
 learn.opt_fn = optim.Adam
 
-learn.lr_find(1e-5, 100)
+lrf=learn.lr_find(1e-5,100)
 
 learn.sched.plot()
 
-#Generally sched.plot skips 10 values at start and 5 at end. Below code will decrease skips 
 learn.sched.plot(n_skip=5, n_skip_end=1)
 
 lr = 2e-2
 
 learn.fit(lr, 1, cycle_len=1)
-
-lrs = np.array([lr/1000,lr/100,lr])
-
-learn.freeze_to(-2)
-
-lrf=learn.lr_find(lrs/1000)
-learn.sched.plot(1)
-
-learn.fit(lrs/5, 1, cycle_len=1)
-
-learn.unfreeze()
-
-learn.fit(lrs/5, 1, cycle_len=2)
-
-learn.save('clas_one')
-
-learn.load('clas_one')
-
-x,y = next(iter(md.val_dl))
-probs = F.softmax(predict_batch(learn.model, x), -1)
-x,preds = to_np(x),to_np(probs)
-preds = np.argmax(preds, -1)
-
-fig, axes = plt.subplots(3, 4, figsize=(12, 8))
-for i,ax in enumerate(axes.flat):
-    ima=md.val_ds.denorm(x)[i]
-    b = md.classes[preds[i]]
-    ax = show_img(ima, axes=ax)
-    draw_text(ax, (0,0), b)
-plt.tight_layout()
-
-BB = PATH/'tmp/bb.csv'
-
-bb = np.array([trn_lrg_anno[o][0] for o in trn_ids])
-bbs = [' '.join(str(p) for p in o) for o in bb]
-df = pd.DataFrame({'fn': [trn_fnm[o] for o in trn_ids],
-                  'bbox': bbs}, columns=['fn', 'bbox'])
-df.to_csv(BB, index=False)
-
-f_model = resnet34
-sz = 224
-bs = 64
-
-augs = [RandomFlip(),
-       RandomRotate(30),
-       RandomLighting(0.1, 0.1)]
-
-tfms = tfms_from_model(f_model, sz, crop_type=CropType.NO, aug_tfms=augs)
-md = ImageClassifierData.from_csv(PATH, JPEGS, BB, bs=4, tfms=tfms, continuous=True)
-
-idx=3
-fig,axes = plt.subplots(3,3, figsize=(9,9))
-for i,ax in enumerate(axes.flat):
-    x,y=next(iter(md.aug_dl))
-    ima=md.val_ds.denorm(to_np(x))[idx]
-    b = bb_hw(to_np(y[idx]))
-    print(b)
-    show_img(ima, axes=ax)
-    draw_rect(ax, b)
-
-tfm_y = TfmType.COORD
-augs = [RandomFlip(tfm_y=tfm_y),
-       RandomRotate(30, tfm_y=tfm_y),
-       RandomLighting(0.1, 0.1, tfm_y=tfm_y)]
-
-tfms = tfms_from_model(f_model, sz, crop_type=CropType.NO, tfm_y=TfmType.COORD, aug_tfms=augs)
-md = ImageClassifierData.from_csv(PATH, JPEGS, BB, bs=4, continuous=True, tfms=tfms)
-
-idx=3
-fig,axes = plt.subplots(3,3, figsize=(9,9))
-for i,ax in enumerate(axes.flat):
-    x,y=next(iter(md.aug_dl))
-    ima=md.val_ds.denorm(to_np(x))[idx]
-    b = bb_hw(to_np(y[idx]))
-    print(b)
-    show_img(ima, axes=ax)
-    draw_rect(ax, b)
-
-# +
-tfm_y = TfmType.COORD
-augs = [RandomFlip(tfm_y=tfm_y),
-        RandomRotate(3, p=0.5, tfm_y=tfm_y),
-        RandomLighting(0.05,0.05, tfm_y=tfm_y)]
-
-tfms = tfms_from_model(f_model, sz, crop_type=CropType.NO, tfm_y=tfm_y, aug_tfms=augs)
-md = ImageClassifierData.from_csv(PATH, JPEGS, BB, tfms=tfms, bs=bs, continuous=True)
-# -
-
-512*7*7
-
-head_reg = nn.Sequential(Flatten(), nn.Linear(25088, 4))
-learn = ConvLearner.pretrained(f_model, md, custom_head=head_reg)
-learn.optim_fn = optim.Adam
-learn.crit = nn.L1Loss()
-
-learn.summary()
-
-learn.lr_find(1e-5,100)
-learn.sched.plot(5)
-
-lr = 2e-3
-
-learn.fit(lr, 2, cycle_len=1, cycle_mult=2)
-
-lrs = np.array([lr/100,lr/10,lr])
-
-learn.freeze_to(-2)
-
-lrf=learn.lr_find(lrs/1000)
-learn.sched.plot(1)
-
-learn.fit(lrs, 2, cycle_len=1, cycle_mult=2)
-
-learn.freeze_to(-3)
-
-learn.fit(lrs, 1, cycle_len=2)
-
-learn.save('reg4')
-
-learn.load('reg4')
-
-x,y = next(iter(md.val_dl))
-learn.model.eval()
-preds = to_np(learn.model(VV(x)))
-
-fig, axes = plt.subplots(3, 4, figsize=(12, 8))
-for i,ax in enumerate(axes.flat):
-    ima=md.val_ds.denorm(to_np(x))[i]
-    b = bb_hw(preds[i])
-    ax = show_img(ima, axes=ax)
-    draw_rect(ax, b)
-plt.tight_layout()
-
-#  ## Single Object Detection
 
 
